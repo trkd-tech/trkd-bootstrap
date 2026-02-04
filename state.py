00dc91b73ef19b0bfaba0d7b1747988665cba31e
trkd-tech <ankitnigam15@gmail.com>
2026-02-04 10:30:23 +0530

    Create state.py

---
+++ state.py
@@ -0,0 +1,14 @@
+# state.py
+
+token_meta = {}
+
+candles_1m = {}
+candles_5m = {}
+last_minute_seen = {}
+
+vwap_state = {}
+opening_range = {}
+
+positions = {}
+
+strategy_state = {}  # token -> strategy -> state
